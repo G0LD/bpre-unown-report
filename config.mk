a358b46c16efb063f37158050a87059088a1eb0f
G0LD <7801558+G0LD@users.noreply.github.com>
2019-09-25 11:49:03 -0600

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # where to start looking for free space
-START_AT = 0x0871A240
+START_AT = 0x09C00000
 
 # the item index to change the field script to
-ITEM_UnownReport = 0x182
+ITEM_UnownReport = 0x167
